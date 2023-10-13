8f93d0d2aff41ac9388a54f93d88f07022aa4de0
Suzette Becraft <suzettebecraft@hotmail.com>
2023-10-13 05:23:28 +0000

    Roll bar with 1 commented line

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -106,8 +106,9 @@ cors = CORS(
 
 # Rollbar-------------------
 rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
-@app.before_first_request
-def init_rollbar():
+#@app.before_first_request
+with app.app_context():
+  def init_rollbar():
     """init rollbar module"""
     rollbar.init(
         # access token
@@ -121,7 +122,7 @@ def init_rollbar():
 
     # send exceptions from `app` to rollbar, using flask's signal system.
     got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
-    
+
 
 @app.route('/rollbar/test')
 def rollbar_test():
