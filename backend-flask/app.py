aff138e04cba4dee5b48d31906d6cbed04e83452
Suzette Becraft <suzettebecraft@hotmail.com>
2023-10-17 03:56:57 +0000

    Comment out Rollbar

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS, cross_origin
 import os
 import sys
 
-from flask_awscognito import AWSCognitoAuthentication
+#from flask_awscognito import AWSCognitoAuthentication
 from services.home_activities import *
 from services.user_activities import *
 from services.create_activity import *
@@ -15,7 +15,7 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
-from lib.cognito_jwt_token import CognitoJwtToken, extract_access_token, TokenVerifyError
+#from lib.cognito_jwt_token import CognitoJwtToken, extract_access_token, TokenVerifyError
 
 # HoneyComb---------
 from opentelemetry import trace
@@ -27,8 +27,8 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
 
 # X-RAY--------------
-from aws_xray_sdk.core import xray_recorder
-from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+#from aws_xray_sdk.core import xray_recorder
+#from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
 # CloudWatch Logs--------
 import watchtower
@@ -57,8 +57,8 @@ processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
 
 # X-RAY---------------
-# xray_url = os.getenv("AWS_XRAY_URL")
-# xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
 
 # OTEL ----------
 # Show this in the logs within the backend-flask app (STDOUT)
@@ -104,35 +104,55 @@ cors = CORS(
 #    return response
 
 
-# Rollbar-------------------
-# Hack to make request data work with pyrollbar <= 0.16.3
-def _get_flask_request():
-    print("Getting flask request")
-    from flask import request
-    print("request:", request)
-    return request
-rollbar._get_flask_request = _get_flask_request
+# Rollbar Original Code ----------
+#rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+#@app.before_first_request
+#def init_rollbar():
+#    """init rollbar module"""
+#    rollbar.init(
+#        # access token
+#        rollbar_access_token,
+#        # environment name
+#        'production',
+        # server root directory, makes tracebacks prettier
+#        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+#        allow_logging_basic_config=False)
 
-def _build_request_data(request):
-    return rollbar._build_werkzeug_request_data(request)
-rollbar._build_request_data = _build_request_data
+    # send exceptions from `app` to rollbar, using flask's signal system.
+#    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
+
+
+# Rollbar Alternate Code Per Andrew-------------------
+# Hack to make request data work with pyrollbar <= 0.16.3
+#def _get_flask_request():
+#    print("Getting flask request")
+#    from flask import request
+#    print("request:", request)
+#    return request
+#rollbar._get_flask_request = _get_flask_request
+
+#def _build_request_data(request):
+#    return rollbar._build_werkzeug_request_data(request)
+#rollbar._build_request_data = _build_request_data
 # End hack
 
-def init_rollbar(app):
-  rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
-  flask_env = os.getenv('FLASK_ENV')
-  rollbar.init(
-      # access token
-      rollbar_access_token,
-      # environment name
-      flask_env,
+#def init_rollbar(app):
+#  rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+#  flask_env = os.getenv('FLASK_ENV')
+#  rollbar.init(
+#      # access token
+#      rollbar_access_token,
+#      # environment name
+#      flask_env,
       # server root directory, makes tracebacks prettier
-      root=os.path.dirname(os.path.realpath(__file__)),
+#      root=os.path.dirname(os.path.realpath(__file__)),
       # flask already sets up logging
-      allow_logging_basic_config=False)
+#      allow_logging_basic_config=False)
   # send exceptions from `app` to rollbar, using flask's signal system.
-  got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
-  return rollbar
+#  got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+#  return rollbar
 
 
 @app.route('/rollbar/test')
